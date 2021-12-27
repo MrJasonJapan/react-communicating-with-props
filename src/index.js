19debd24d4384e6b914fb5121bf349bf1a5e54d7
SpaGettys <jgettys@gmail.com>
2021-12-27 23:14:18 +0900

    Insert in a single comment and generate an avatar (animals) image with faker.js
    <notes>
    npm install --save faker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.animals()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-  // Take the react compoent and show it on the scren
-ReactDOM.render(<App />, document.querySelector('#root'));
+// Take the react compoent and show it on the scren
+ReactDOM.render(<App />, document.querySelector("#root"));
