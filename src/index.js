55eaa356df8823418128b78df6d4a3bf579360da
SpaGettys <jgettys@gmail.com>
2021-12-27 22:38:47 +0900

    Initial Working commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+    return <div>Hi there!</div>
+};
+
+  // Take the react compoent and show it on the scren
+ReactDOM.render(<App />, document.querySelector('#root'));
