96d6ccab45a57be9a4e9990a1f73c54f38902151
SpaGettys <jgettys@gmail.com>
2021-12-28 16:13:42 +0900

    Wrap each comment into an approval card, by taking advantage of a separate reusable Component called ApprovalCard. Make it so we can pass in CommentDetail as a “children prop” to ApprovalCard.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:15PM"
-        content="Nice blog post!"
-        avatar={faker.image.animals()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 1:15AM"
-        content="Even better post!"
-        avatar={faker.image.animals()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 6:25PM"
-        content="The best post!"
-        avatar={faker.image.animals()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>Are you sure you want to do this?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:15PM"
+          content="Nice blog post!"
+          avatar={faker.image.animals()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 1:15AM"
+          content="Even better post!"
+          avatar={faker.image.animals()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 6:25PM"
+          content="The best post!"
+          avatar={faker.image.animals()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
